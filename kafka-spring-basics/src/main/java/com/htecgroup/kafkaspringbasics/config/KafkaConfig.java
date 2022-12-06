407783ff426a24f39ccbe6e82b1b4dcb562409c4
qnenadpejovic <nenad.pejovic@quinyx.com>
2022-12-06 16:13:24 +0100

    fixed imports

--- kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/config/KafkaConfig.java
+++ kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/config/KafkaConfig.java
@@ -2,23 +2,19 @@ package com.htecgroup.kafkaspringbasics.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.ContainerProperties.AckMode;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 @Slf4j
 @Configuration
