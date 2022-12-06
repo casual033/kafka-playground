8724059cffb49cbc595129103fcd18c649311baa
qnenadpejovic <nenad.pejovic@quinyx.com>
2022-12-06 16:21:27 +0100

    fixed tests

--- kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/listeners/TestTopicBasicListener.java
+++ kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/listeners/TestTopicBasicListener.java
@@ -25,7 +25,7 @@ public class TestTopicBasicListener {
 
       log.info("Message received: {}", message);
       if(message.equals("msg2")) {
-          Thread.sleep(6000);
+          Thread.sleep(5000);
       }
       acknowledgment.acknowledge();
       messages.add(message);
