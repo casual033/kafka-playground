e34adbf7933209eb3f6883ca90456135093b3c0d
qnenadpejovic <nenad.pejovic@quinyx.com>
2022-12-28 13:26:55 +0100

    Minor fixes to a showcase example

--- kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/controllers/ProduceMessageController.java
+++ kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/controllers/ProduceMessageController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/produce")
+@RequestMapping(value = "/producer")
 @Slf4j
 @AllArgsConstructor
 public class ProduceMessageController {
 
   private final BasicProducer basicProducer;
 
-  @PostMapping(value = "/basic", consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/message", consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity produceMessage(@RequestBody String message) {
     basicProducer.send(message);
     return ResponseEntity.ok().build();
--- kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/listeners/TestTopicBasicListener.java
+++ kafka-spring-basics/src/main/java/com/htecgroup/kafkaspringbasics/listeners/TestTopicBasicListener.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -14,7 +15,8 @@ public class TestTopicBasicListener {
       topics = {"${topicConfig.testTopic}"},
       groupId = "${spring.kafka.consumer.group-id}" + "-basic"
   )
-  public void onMessage(String message) {
-    log.info("Consumer new message: {}", message);
+  public void onMessage(String message, Acknowledgment acknowledgment) {
+    log.info("Consumed new message: {}", message);
+    acknowledgment.acknowledge();
   }
 }
